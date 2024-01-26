10ed25e97548e65d68cf952a84c62a2dc46863bc
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2024-01-26 11:01:03 +0800

    feat: add GET and POST to /api/books route

--- routes/api.js
+++ routes/api.js
@@ -1,47 +1,75 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+'use strict'
+
+require('../connection')
+const Book = require('../schema')
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route('/api/books')
+    .get(async (req, res) => {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      try {
+        let books = await Book.find()
+
+        books = books.map((item) => {
+          console.log(item)
+          return {
+            _id: item._id,
+            title: item.title,
+            comments: item.comments,
+            commentcount: item.comments.length,
+          }
+        })
+
+        return res.json(books)
+      } catch (err) {
+        return res.json(err)
+      }
     })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+
+    .post(async (req, res) => {
+      let title = req.body.title
+
+      // response will contain new book object including atleast _id and title
+      if (!title) return res.send('missing required field title')
+
+      try {
+        let book = new Book({ title })
+        book = await book.save()
+        res.json(book)
+      } catch (err) {
+        return res.json(err)
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete(async (req, res) => {
       //if successful response will be 'complete delete successful'
-    });
+    })
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
+  app
+    .route('/api/books/:id')
+    .get(async (req, res) => {
+      let bookid = req.params.id
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
+
+    .post(async (req, res) => {
+      let bookid = req.params.id
+      let comment = req.body.comment
+      console.log(bookid, comment)
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
+
+    .delete(async (req, res) => {
+      let bookid = req.params.id
       //if successful response will be 'delete successful'
-    });
-  
-};
+    })
+}
